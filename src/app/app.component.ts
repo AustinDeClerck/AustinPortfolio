9fdbef156a1d36883c5a48a9ba733a88fa54e502
Austin <declerckaustin@gmail.com>
2025-01-09 18:21:40 -0600

    images and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [ RouterOutlet, NavComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
