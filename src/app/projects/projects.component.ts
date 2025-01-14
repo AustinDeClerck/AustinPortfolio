1fc86186fb70f9aa420d915b45b137907803ae17
Austin <declerckaustin@gmail.com>
2025-01-14 10:18:26 -0600

    project card update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ProjectCardComponent } from "../project-card/project-card.component";
+import { Project } from '../_models/project';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  project: Project = {
+    id: 0,
+    name: 'Sample Angular App',
+    summary: 'Test Description',
+    description: '',
+    projectLink: '',
+    tags: ['Angular', 'Typescript'],
+    pictures: []
+  }
 }
