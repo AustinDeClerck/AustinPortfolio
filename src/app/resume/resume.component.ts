1400d864f0dd854fda4edc90b74ac0c5e5f20ba7
Austin <declerckaustin@gmail.com>
2025-01-17 19:26:56 -0600

    resume completeion and styling

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [ AccordionModule ],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
 
+  constructor(private renderer: Renderer2) {
+
+  }
+
+  DownloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'Resume.pdf');
+    link.setAttribute('download', 'Resume.pdf');
+    link.click();
+    link.remove();
+  }
 }
